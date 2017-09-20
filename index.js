446a23402d1ddde083096cfd1719fa3deb82533b
Eric Roginek <eroginek@protonmail.com>
2017-09-20 19:21:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-    var remove = Object.assign({}, object, {[key]})
+    var remove = Object.assign({}, object)
     return delete remove.key
 }
